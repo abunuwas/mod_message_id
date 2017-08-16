e036e2251e66889fa04f793ba35aad039bf86e8e
Cloud User <centos@ip-10-10-1-52.eu-west-1.compute.internal>
2017-08-16 12:45:42 +0000

    SW-1995: Update compile.sh to do all the job

--- compile.sh
+++ compile.sh
@@ -1,8 +1,34 @@
+#!/bin/bash
 
-#clone eredis
-#cd eredis && ./rebar compile
-#cp -r eredis /usr/local/lib/erlang/lib/
-#clone xml, compile
+cd ~
+
+if [ -d eredis_pool ]; then
+    rm -rf eredis_pool
+fi
+
+if [ -d xmpp ]; then
+    rm -rf xmpp
+fi
+
+git clone https://github.com/hiroeorz/eredis_pool.git eredis_pool
+pushd eredis_pool 
+make get-deps || exit 1
+make || exit 1
+
+sudo cp -r deps/eredis /usr/local/lib/erlang/lib/
+sudo cp -r deps/poolboy /usr/local/lib/erlang/lib/
+
+popd
+
+sudo cp -r eredis_pool /usr/local/lib/erlang/lib/
+
+git clone https://github.com/processone/xmpp.git
+pushd xmpp 
+make || exit 1
+popd
+sudo cp -r xmpp /usr/local/lib/erlang/lib/
+
+cd mod_message_id
 
 mkdir -p ebin
 erlc -d -I /lib/ejabberd-*/include \
@@ -12,8 +38,8 @@ erlc -d -I /lib/ejabberd-*/include \
         -pa /home/centos/eredis_pool/deps/eredis/ebin \
         -pa /home/centos/eredis_pool/deps/pool/ebin \
         -pa /home/centos/eredis_pool/ebin \
-        -o ebin src/mod_message_id.erl && 
-cp ebin/mod_message_id.beam /lib/ejabberd-16*/ebin &&
-cp /etc/ejabberd/ejabberd.yml /etc/ejabberd/ejabberd.yml.bk &&
-sed -i "s/mod_restful:/mod_message_id: {}\n  mod_restful:/" /etc/ejabberd/ejabberd.yml && 
-su - ejabberd -c "ejabberdctl restart"
+        -o ebin src/mod_message_id.erl || exit 1
+sudo cp ebin/mod_message_id.beam /lib/ejabberd-16*/ebin || exit 1
+sudo cp /etc/ejabberd/ejabberd.yml /etc/ejabberd/ejabberd.yml.bk || exit 1
+sudo sed -i "s/mod_restful:/mod_message_id: {}\n  mod_restful:/" /etc/ejabberd/ejabberd.yml || exit 1
+sudo su - ejabberd -c "ejabberdctl restart"
